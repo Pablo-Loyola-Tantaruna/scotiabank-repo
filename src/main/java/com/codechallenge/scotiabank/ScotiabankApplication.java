0365e95d9174dc3bfa8696597d615553ffc7996b
Ployola <ployola@hiper.com.pe>
2024-06-11 02:57:28 -0500

    feat: add comments to checkstyle

--- src/main/java/com/codechallenge/scotiabank/ScotiabankApplication.java
+++ src/main/java/com/codechallenge/scotiabank/ScotiabankApplication.java
@@ -3,9 +3,38 @@ package com.codechallenge.scotiabank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p> Clase principal de la aplicación.</p>
+ * <p> Esta clase es la encargada de iniciar la aplicación.</p>
+ * <p><b>Class</b>: ScotiabankApplication</p>
+ * <p><b>Package</b>: com.codechallenge.scotiabank</p>
+ * <p><b>Project</b>: codechallenge</p>
+ * <p><b>Version</b>: 1.0.0</p>
+ * <p><b>Company</b>: Encore</p>
+ * <p><b>Creation Date</b>: 2024-06-10</p>
+ * <p>@author Pablo Sergio Loyola Tantaruna</p>
+ * <div>
+ *   <u>Developed by</u>:
+ *   <ul>
+ *     <li>Pablo Sergio Loyola Tantaruna</li>
+ *   </ul>
+ * </div>
+ * <div>
+ *   <u>Reviewed by</u>:
+ *   <ul>
+ *     <li>Pablo Sergio Loyola Tantaruna</li>
+ *   </ul>
+ * </div>
+ * <p><b>Version</b>: 1.0.0</p>
+ */
 @SpringBootApplication
 public class ScotiabankApplication {
 
+  /**
+   * Método principal de la aplicación.
+   *
+   * @param args Argumentos de la aplicación.
+   */
   public static void main(String[] args) {
     SpringApplication.run(ScotiabankApplication.class, args);
   }
