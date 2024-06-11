732df24d69666c4073af8fd062982740a7e119a1
Ployola <ployola@hiper.com.pe>
2024-06-11 02:53:45 -0500

    fix: identation to checkstyles

--- src/test/java/com/codechallenge/scotiabank/ScotiabankApplicationTests.java
+++ src/test/java/com/codechallenge/scotiabank/ScotiabankApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ScotiabankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
