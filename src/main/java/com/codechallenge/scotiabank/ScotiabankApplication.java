dec3c607c42d76efff8681a2475174c274d3063c
Ployola <ployola@hiper.com.pe>
2024-06-11 02:29:22 -0500

    init: This project is for an interview an scotiabank

---
+++ src/main/java/com/codechallenge/scotiabank/ScotiabankApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.scotiabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotiabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotiabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallenge/scotiabank/ScotiabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.scotiabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotiabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
